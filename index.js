01f64f7fed7177c9fc9655c378b98caa0ed755b8
Tausif <tausif00100@gmail.com>
2025-05-20 02:41:36 +0530

    completed language translation tool

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import {ChatGoogleGenerativeAI} from "@langchain/google-genai"
-
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import readline from 'readline'
 console.log("KEY :", process.env.GOOGLE_GEMINI_API_KEY);
 
 // now instantitate: to creating object from class
@@ -10,5 +11,20 @@ console.log("KEY :", process.env.GOOGLE_GEMINI_API_KEY);
 const model = new ChatGoogleGenerativeAI({
 model: "gemini-2.0-flash",
 // randomness of output: 0 low and 2 is high 
-temperature:0
+temperature:0,
+// apiKey: 'AIzaSyDAf6Q5bINmO3lgBY41zbnw3g_jffmiwN8',
+apiKey: process.env.GOOGLE_GEMINI_API_KEY,
 })
+
+const messages = [
+  new SystemMessage("Translate the following from English into HINDI"),
+  new HumanMessage("Hello my name is Tausif"),
+];
+
+// This is for invoking the AI model 
+const output = await model.invoke(messages)
+
+// Don't do basic mistakes: for seeing your
+//  result you have to console it 
+console.log("OUTPUT :",output.content);
+
