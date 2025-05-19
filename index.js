59e00d008ff05799bd9e607267157148a355f5d2
Tausif <tausif00100@gmail.com>
2025-05-20 01:53:55 +0530

    instantiate the model

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import 'dotenv/config'
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai"
 
 console.log("KEY :", process.env.GOOGLE_GEMINI_API_KEY);
+
+// now instantitate: to creating object from class
+// Actually this ChatGoogleGenerativeAi is class 
+// by using new keyword we create it object 
+
+const model = new ChatGoogleGenerativeAI({
+model: "gemini-2.0-flash",
+// randomness of output: 0 low and 2 is high 
+temperature:0
+})
